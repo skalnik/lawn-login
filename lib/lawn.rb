c42287c428af0ab1007ae22c5e0156a02f201e91
Mike Skalnik <mike.skalnik@gmail.com>
2008-09-24 13:56:43 -0400

    Now using EzCrypto library instead of crypt/blowfish.

--- lib/lawn.rb
+++ lib/lawn.rb
@@ -1,7 +1,8 @@
-require 'crypt/blowfish'
+require 'rubygems'
+require 'ezcrypto'
 module Lawn
   extend self
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
   
   def get_encryption_password(username)
     if username.length < 7
@@ -12,7 +13,10 @@ module Lawn
   end
   
   def encrypt_password(encryption_password, password)
-    blowfish = Crypt::Blowfish.new(encryption_password)
-    blowfish.encrypt_block(password)
+    EzCrypto::Key.encrypt_with_password encryption_password, "salt", password
+  end
+  
+  def decrypt_password(encryption_password, password)
+    EzCrypto::Key.decrypt_with_password encryption_password, "salt", password
   end
 end
